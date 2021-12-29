998bb2b2100e7457e7effc1c65711d22d5187482
nick <2351536460@qq.com>
2021-12-29 01:00:22 -0800

    final

--- app.js
+++ app.js
@@ -59,6 +59,7 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   console.log(err)
   
+  
   res.status(err.status || 500);
   res.render('error');
 });
